9f4b38ffc859b6d87d122f1e4eadd4fe6c22b804
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-10-18 13:55:50 +0900

    Drag and drop rows+create line tab+delete on backspace

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,14 @@ var sockRageControllers = angular.module('ScrumControllers', ['sockRage', 'ngGri
 sockRageControllers.controller('dashboardController', ['$scope', '$AngularSockr',
     function ($scope, $AngularSockr) {
 
+        $scope.globals = {
+            justAdded : false,
+            justDeleted : false,
+            justUpdated : false,
+            removeTap : 0,
+            updateTimeout : 0
+        };
+
         var ref = new SockRage(appConfig.sockrage_addr, appConfig.scrum_item_collection); //Create a reference
 
         var sync = $AngularSockr(ref); //create a $SockRageAngular instance
@@ -24,15 +32,19 @@ sockRageControllers.controller('dashboardController', ['$scope', '$AngularSockr'
             enableColumnResize: false,
             enableColumnReordering: false,
             enableCellEditOnFocus: true,
+            enableRowReordering: true,
+            enableRowSelection: true,
+            multiSelect: false,
             columnDefs: [
-                {field: '_id', displayName: 'ID', enableCellEdit: false, sortable: false},
+                {field: 'order_id', displayName: '#', enableCellEdit: false, sortable: false},
                 {field: 'task_description', displayName: 'Task', enableCellEdit: true, sortable: false},
                 {field: 'owner', displayName: 'Owner', enableCellEdit: true, sortable: false},
                 {field: 'time_estimate', displayName: 'Time estimate', enableCellEdit: false, cellTemplate: $scope.cellSelectEditableTemplateTimeEstimate, sortable: false},
                 {field: 'time_reported', displayName: 'Time reported', enableCellEdit: false, sortable: false, cellTemplate: $scope.cellSelectEditableTemplateTimeReported},
-                {field: 'state', displayName: 'State', enableCellEdit: true, cellTemplate: $scope.cellSelectEditableTemplateState, enableCellEdit: false, sortable: false},
-                {displayName: 'Options', cellTemplate: $scope.optionsTemplate, sortable: false, enableCellEdit: false}
-            ]
+                {field: 'state', displayName: 'State', enableCellEdit: true, cellTemplate: $scope.cellSelectEditableTemplateState, enableCellEdit: false, sortable: false}
+            ],
+            plugins: [ new ngGridDraggableRow('_id'), new ngGridFlexibleHeightPlugin() ],
+            selectedItems: []
         };
 
         $scope.scrumItem = {};
@@ -45,6 +57,107 @@ sockRageControllers.controller('dashboardController', ['$scope', '$AngularSockr'
             $scope.updateScrumItemInline(entity);
         });
 
+        $scope.$on('ngGridEventData', function (evt) {
+
+            var scrumItemsCache = angular.copy($scope.scrumItems);
+
+            //sort by order ID
+            for(var i = 0; i < scrumItemsCache.length; i++) {
+
+                var itemCache = scrumItemsCache[i];
+                $scope.scrumItems[itemCache.order_id] = itemCache;
+
+                console.log("assign = " + itemCache.order_id + " to " + itemCache.task_description);
+            }
+
+        });
+
+        $scope.$on('ngGridEventRows', function (evt) {
+
+            //Just added a row, scroll down + set selected the last row.
+            if($scope.globals.justAdded) {
+
+                $scope.gridScrumItemsOptions.selectRow($scope.scrumItems.length - 1, true);
+                window.scrollTo(0, document.body.scrollHeight);
+
+                $scope.globals.justAdded = false;
+            }
+
+            if($scope.globals.justDeleted) {
+
+                console.log("del");
+
+                $scope.updateItemKeys();
+
+                $scope.globals.justDeleted = false;
+            }
+
+        });
+
+        $scope.updateItemKeys = function() {
+
+            console.log($scope.gridScrumItemsOptions.ngGrid);
+
+            for(var i = 0; i < $scope.gridScrumItemsOptions.ngGrid.data.length; i++) {
+
+                var item = $scope.gridScrumItemsOptions.ngGrid.data[i];
+
+                item = angular.copy(item);
+
+                var id = item._id;
+                delete item['_id'];
+                item.order_id = i;
+
+                console.log("update " + item.task_description + " to " + i);
+
+                $scope.scrumItems.$update(id, item);
+            }
+
+            //console.log($scope.scrumItems);
+
+        }
+
+        $scope.keyupOnTable = function(e) {
+
+            if ($scope.gridScrumItemsOptions.selectedItems[0]) {
+
+                var selectedItem = $scope.gridScrumItemsOptions.selectedItems[0];
+
+                if (e.keyCode == 8
+                    && selectedItem.task_description.length == 0) {
+
+                    if($scope.globals.removeTap > 0) { //We only delete an entry on double tap !
+                        $scope.deleteScrumItem(selectedItem._id);
+                        $scope.globals.removeTap = 0;
+                    }
+                    else {
+                        $scope.globals.removeTap++;
+                        setTimeout(function() {
+                            $scope.globals.removeTap = 0;
+                        }, 500);
+                    }
+
+                }
+                else if(e.keyCode == 9) {
+                    $scope.globals.justAdded = true;
+                    $scope.addAnonymousScrumItem();
+                }
+
+            }
+
+        }
+
+        $scope.addAnonymousScrumItem = function() {
+            $scope.scrumItems.$set({
+                order_id: $scope.scrumItems.length,
+                task_description: "NEW TASK",
+                owner: "ANONYMOUS",
+                time_estimate: 0,
+                time_reported: 0,
+                state: "To do"
+            });
+        }
+
         $scope.addScrumItem = function (scrumItem) {
 
             if ($scope.createScrumItemForm.$valid) {
@@ -55,6 +168,7 @@ sockRageControllers.controller('dashboardController', ['$scope', '$AngularSockr'
                 else {
 
                     $scope.scrumItems.$set({
+                        order_id: $scope.scrumItems.length,
                         task_description: scrumItem.task_description,
                         owner: scrumItem.owner,
                         time_estimate: scrumItem.time_estimate,
@@ -74,49 +188,17 @@ sockRageControllers.controller('dashboardController', ['$scope', '$AngularSockr'
 
         $scope.updateScrumItemInline = function (scrumItem) {
 
-            $scope.scrumItems.$update(scrumItem._id, {
-                task_description: scrumItem.task_description,
-                owner: scrumItem.owner,
-                time_estimate: scrumItem.time_estimate,
-                time_reported: scrumItem.time_reported,
-                state: scrumItem.state
-            });
+            var id = scrumItem._id;
+            delete scrumItem['_id'];
 
-        }
+            $scope.scrumItems.$update(id, scrumItem);
 
-        $scope.updateScrumItem = function (scrumItem) {
-
-            if ($scope.updateScrumItemForm.$valid) {
-
-                $scope.scrumItems.$update(scrumItem._id, {
-                    task_description: scrumItem.task_description,
-                    owner: scrumItem.owner,
-                    time_estimate: scrumItem.time_estimate,
-                    time_reported: scrumItem.time_reported,
-                    state: scrumItem.state
-                });
-
-                $scope.scrumItemToUpdate = {};
-
-                $('#modalUpdateScrumItem').modal('hide');
-
-            }
-            else {
-                toastr.error("Invalid form. Please check your fields");
-            }
-
-        }
-
-        $scope.openUpdateModalWindow = function (scrumItem) {
-
-            $("#modalUpdateScrumItemLabel").html("Update task " + scrumItem._id);
-
-            $scope.scrumItemToUpdate = angular.copy(scrumItem);
         }
 
         $scope.deleteScrumItem = function (scrumItem_id) {
 
             bootbox.confirm("Are you sure you want to delete this item ?", function (result) {
+                $scope.globals.justDeleted = true;
                 $scope.scrumItems.$delete(scrumItem_id);
             });
 
