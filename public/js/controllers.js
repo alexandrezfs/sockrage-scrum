43e6757d1bd169ffc40dc5deabd2b448899ecfd0
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-10-27 17:36:40 +0900

    Can move up/down with keyboard

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,143 +1,148 @@
 var scrumControllers = angular.module('ScrumControllers', []);
 
 scrumControllers.controller('dashboardController', ['$scope', '$timeout', '$interval',
-    function ($scope, $timeout, $interval) {
+        function ($scope, $timeout, $interval) {
 
-        var ref = new SockRage(appConfig.sockrage_addr, appConfig.scrum_item_collection); //Create a reference
+            var ref = new SockRage(appConfig.sockrage_addr, appConfig.scrum_item_collection); //Create a reference
 
-        $scope.states = ["To do", "Checkout", "Done"];
-        $scope.hours = [];
-        for (var i = 0; i < 501; i++) {
-            $scope.hours.push(i);
-        }
-
-        $scope.scrumItems = [];
-
-        $interval(function () {
-
-            if ($scope.scrumItems.length == 0
-                || $scope.scrumItems[$scope.scrumItems.length - 1].task_description != "") {
-
-                $scope.addAnonymousItemIfNotExists();
+            $scope.states = ["To do", "Checkout", "Done"];
+            $scope.hours = [];
+            for (var i = 0; i < 501; i++) {
+                $scope.hours.push(i);
             }
-
-        }, 500);
-
-        ref.on('getAll', function (data) {
 
             $scope.scrumItems = [];
 
-            for (var i = 0; i < data.length; i++) {
-                var item = data[i];
-                $scope.scrumItems[item.order_index] = item;
-            }
+            $interval(function () {
 
-            $scope.$apply();
+                if ($scope.scrumItems.length == 0
+                    || $scope.scrumItems[$scope.scrumItems.length - 1].task_description != "") {
 
-        });
+                    $scope.addAnonymousItemIfNotExists();
+                }
 
-        ref.on("create", function (data) {
+            }, 500);
 
-            $scope.scrumItems.push(data);
+            $scope.setSelectedRow = function (selectedId) {
+                $scope.selectedId = selectedId;
+            };
 
-            $scope.$apply();
+            ref.on('getAll', function (data) {
 
-            //if the pushed data was not an anonymous data...
+                $scope.scrumItems = [];
 
-            if (data.task_description && data.task_description.length != ""
-                && $scope.scrumItems[$scope.scrumItems.length - 1].task_description.length > 0) {
+                for (var i = 0; i < data.length; i++) {
+                    var item = data[i];
+                    $scope.scrumItems[item.order_index] = item;
+                }
 
-                $scope.addAnonymousItemIfNotExists();
-            }
-        });
+                $scope.$apply();
 
-        ref.on("update", function (data) {
-
-            //updating the key
-            $scope.scrumItems[data.data.order_index] = data.data;
-
-            $scope.$apply();
-
-        });
-
-        ref.on("delete", function(data) {
-
-            //everyone has to remove it from their index.
-            $scope.scrumItems.splice(data.data.order_index, 1);
-            updateOrderIndexLocal();
-
-            //the remote arrays has to be updated with new indexes
-
-            updateArrayRemote();
-
-        });
-
-        $scope.addAnonymousItemIfNotExists = function () {
-
-            ref.set({
-                task_description: "",
-                owner: "",
-                time_estimate: 1,
-                time_reported: 1,
-                state: "To do",
-                order_index: $scope.scrumItems.length
             });
 
-        }
+            ref.on("create", function (data) {
 
-        $scope.sortableOptions = {
-            stop: function (e, ui) {
-                /*
-                 var item = ui.item.scope().item;
-                 var fromIndex = ui.item.sortable.index;
-                 var toIndex = ui.item.sortable.dropindex;
-                 */
+                $scope.scrumItems.push(data);
+                $scope.$apply();
 
-                //updating new array key ==> order_index key
+                //if the pushed data was not an anonymous data...
+                if (data.task_description && data.task_description.length != ""
+                    && $scope.scrumItems[$scope.scrumItems.length - 1].task_description.length > 0) {
+
+                    $scope.addAnonymousItemIfNotExists();
+                }
+            });
+
+            ref.on("update", function (data) {
+
+                //updating the key
+                $scope.scrumItems[data.data.order_index] = data.data;
+                $scope.$apply();
+            });
+
+            ref.on("delete", function (data) {
+
+                //everyone has to remove it from their index.
+                $scope.scrumItems.splice(data.data.order_index, 1);
                 updateOrderIndexLocal();
 
-                //and then, when it's done, send it to the remote.
+                //the remote arrays has to be updated with new indexes
                 updateArrayRemote();
-            }
-        };
+            });
 
-        $scope.updateScrumItem = function (item) {
+            $scope.addAnonymousItemIfNotExists = function () {
 
-            ref.update(item._id, item);
-        }
-
-        $scope.removeScrumItem = function(event, item) {
-
-            if(event.keyCode == 8) {
-
-                var _id = item._id;
-
-                //remote update
-                ref.delete(_id);
+                ref.set({
+                    task_description: "",
+                    owner: "",
+                    time_estimate: 1,
+                    time_reported: 1,
+                    state: "To do",
+                    order_index: $scope.scrumItems.length
+                });
 
             }
 
-        }
+            $scope.sortableOptions = {
+                stop: function (e, ui) {
+                    /*
+                     var item = ui.item.scope().item;
+                     var fromIndex = ui.item.sortable.index;
+                     var toIndex = ui.item.sortable.dropindex;
+                     */
 
-        function updateOrderIndexLocal() {
+                    //updating new array key ==> order_index key
+                    updateOrderIndexLocal();
 
-            //updating new array key ==> order_index key
-            for (var i = 0; i < $scope.scrumItems.length; i++) {
-                var item = $scope.scrumItems[i];
-                item.order_index = i;
-            }
-        }
+                    //and then, when it's done, send it to the remote.
+                    updateArrayRemote();
+                }
+            };
 
-        function updateArrayRemote() {
-
-            for (var i = 0; i < $scope.scrumItems.length; i++) {
-
-                var item = $scope.scrumItems[i];
+            $scope.updateScrumItem = function (item) {
 
                 ref.update(item._id, item);
             }
-        }
 
-        ref.list();
-    }]
+            $scope.removeScrumItem = function (event, item) {
+
+                if (event.keyCode == 8) {
+                    ref.delete(item._id);
+                }
+            }
+
+            $scope.navigateRow = function (event, index) {
+
+                if (event.keyCode == 38) { //move up
+                    index--;
+                    $("#input-description-" + index).focus();
+                }
+                else if (event.keyCode == 40) { //move down
+                    index++;
+                    $("#input-description-" + index).focus();
+                }
+
+            }
+
+            function updateOrderIndexLocal() {
+
+                //updating new array key ==> order_index key
+                for (var i = 0; i < $scope.scrumItems.length; i++) {
+                    var item = $scope.scrumItems[i];
+                    item.order_index = i;
+                }
+            }
+
+            function updateArrayRemote() {
+
+                for (var i = 0; i < $scope.scrumItems.length; i++) {
+
+                    var item = $scope.scrumItems[i];
+
+                    ref.update(item._id, item);
+                }
+            }
+
+            ref.list();
+        }]
 );
